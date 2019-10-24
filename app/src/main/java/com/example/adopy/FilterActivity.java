a42725adffd6fe2498d41ac237664131343ede58
Itaye Zbit <Lordzbit666@gmail.com>
2019-10-24 21:57:02 +0300

    Filter page restored! MultipleSelectionSpinner, SearchPreferences added

--- app/src/main/java/com/example/adopy/FilterActivity.java
+++ app/src/main/java/com/example/adopy/FilterActivity.java
@@ -2,15 +2,38 @@ package com.example.adopy;
 
 import android.os.Bundle;
 
+import com.example.adopy.UI_utilities.MultipleSelectionSpinner;
+import com.example.adopy.Utilities.SearchPreferences;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.jaygoo.widget.RangeSeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.honorato.multistatetogglebutton.MultiStateToggleButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilterActivity extends AppCompatActivity {
+
+    private static final String TAG = "my_FilterActivity";
+
+    //MultipleSelection spinner object
+    MultipleSelectionSpinner typeSpinner;
+
+    //List which hold multiple selection spinner values
+    List<String> typeSpinnerList = new ArrayList<String>();
+
+    RangeSeekBar ageSeekbar;
+    RangeSeekBar distanceSeekbar;
+    MultiStateToggleButton sex_mstb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +42,65 @@ public class FilterActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //typeSpinner START
+        //casting of spinner
+        typeSpinner = findViewById(R.id.typeSpinner);
+
+        //adding items to typeSpinnerList TODO localais & other
+        typeSpinnerList.add("Dog");
+        typeSpinnerList.add("Cat");
+        typeSpinnerList.add("Rabbit");
+        typeSpinnerList.add("Hedgehog");
+        typeSpinnerList.add("Chinchilla");
+        typeSpinnerList.add("Iguana");
+        typeSpinnerList.add("Turtle");
+
+        //set items to spinner from typeSpinnerList
+        typeSpinner.setItems(typeSpinnerList);
+        //typeSpinner END
+
+        ageSeekbar = findViewById(R.id.ageSeekBar);
+        ageSeekbar.setIndicatorTextDecimalFormat("0");
+        ageSeekbar.setProgress(0, 25);
+
+        distanceSeekbar = findViewById(R.id.distanceSeekBar);
+        distanceSeekbar.setIndicatorTextDecimalFormat("0");
+        distanceSeekbar.setProgress(100);
+
+        sex_mstb = findViewById(R.id.sex_mstb);
+        sex_mstb.setValue(1);
+
+        Button saveBtn = findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                savePreferences();
             }
         });
+
+        Button cancelBtn = findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO are you sure Dialog
+                Log.d(TAG, "canceled");
+                FilterActivity.this.finish();
+            }
+        });
+    }
+
+    private void savePreferences() {
+        SearchPreferences sp = new SearchPreferences();
+        sp.setTypes(typeSpinner.getSelectedStrings());
+        sp.setAgeMin(Math.round(ageSeekbar.getLeftSeekBar().getProgress()));
+        sp.setAgeMax(Math.round(ageSeekbar.getRightSeekBar().getProgress()));
+        sp.setSex(getResources().getStringArray(R.array.sex_array)[sex_mstb.getValue()]);
+        sp.setDistance(Math.round(distanceSeekbar.getLeftSeekBar().getProgress()));
+
+        String ans = "types:" + sp.getTypes() + "\nage: " + sp.getAgeMin() + " - " + sp.getAgeMax() + "\nSex: " + sp.getSex() + "\nDistance: " + sp.getDistance();
+        Log.d(TAG, "savePreferences: \n" + ans);
+        Toast.makeText(this, "" + ans, Toast.LENGTH_SHORT).show();
+
+        FilterActivity.this.finish();
     }
 }
---
+++ app/src/main/java/com/example/adopy/Utilities/SearchPreferences.java
@@ -0,0 +1,55 @@
+package com.example.adopy.Utilities;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SearchPreferences implements Serializable {
+    List<String> types;
+    int ageMin;
+    int ageMax;
+    String sex;
+    int distance;
+
+    public SearchPreferences() {
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<String> types) {
+        this.types = types;
+    }
+
+    public int getAgeMin() {
+        return ageMin;
+    }
+
+    public void setAgeMin(int ageMin) {
+        this.ageMin = ageMin;
+    }
+
+    public int getAgeMax() {
+        return ageMax;
+    }
+
+    public void setAgeMax(int ageMax) {
+        this.ageMax = ageMax;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+}
