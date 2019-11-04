1430007da8a105ab3e527e7377eece5273943f3f
Itaye Zbit <Lordzbit666@gmail.com>
2019-11-04 11:01:14 +0200

    show last msg WORK

--- app/src/main/java/com/example/adopy/UI_utilities/Adapters/UserAdapter.java
+++ app/src/main/java/com/example/adopy/UI_utilities/Adapters/UserAdapter.java
@@ -13,8 +13,15 @@ import com.example.adopy.ChatActivity2;
 import com.example.adopy.ChatsActivity;
 import com.example.adopy.MainActivity;
 import com.example.adopy.R;
+import com.example.adopy.Utilities.Models.Chat;
 import com.example.adopy.Utilities.Models.User;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -26,6 +33,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     private Context mContext;
     private List<User> mUsersList;
 
+    String theLastMessage;
+
     public UserAdapter(Context mContext, List<User> mChatsList) {
         this.mContext = mContext;
         this.mUsersList = mChatsList;
@@ -34,8 +43,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
-            return new UserAdapter.ViewHolder(view);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new UserAdapter.ViewHolder(view);
     }
 
     @Override
@@ -62,6 +71,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
                 mContext.startActivity(intent);
             }
         });
+
+        lastMessage(user.getId(), holder.last_msg);
     }
 
     @Override
@@ -72,6 +83,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView username;
         public ImageView profile_image;
+        private TextView last_msg;
 
 
         public ViewHolder(@NonNull View itemView) {
@@ -79,6 +91,40 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
             username = itemView.findViewById(R.id.username);
             profile_image = itemView.findViewById(R.id.profile_image);
+            last_msg = itemView.findViewById(R.id.last_msg);
         }
+    }
+
+    //check for last msg
+    private void lastMessage(final String userId, final TextView last_msg) {
+        theLastMessage = "default";
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Chat chat = snapshot.getValue(Chat.class);
+                    if (chat.getReceiver().equals(firebaseUser.getUid()) && chat.getSender().equals(userId) ||
+                            chat.getReceiver().equals(userId) && chat.getSender().equals(firebaseUser.getUid())) {
+                        theLastMessage = chat.getMessage();
+                    }
+                }
+                switch (theLastMessage) {
+                    case "default":
+                        last_msg.setText("No Message");
+                        break;
+                    default:
+                        last_msg.setText(theLastMessage);
+                        break;
+                }
+                theLastMessage = "default";
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
