8a7a984b8a2f359e076960d9e4b313ff41c12f5d
Inbal Schkolnik <capoeira0012>
2019-11-25 23:33:46 +0200

    colon text added, style/ProfileTextView,

--- app/src/main/java/com/example/adopy/Activities/PetPageActivity.java
+++ app/src/main/java/com/example/adopy/Activities/PetPageActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
